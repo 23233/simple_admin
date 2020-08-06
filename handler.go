df1b3ef5703172ab39cd9f1f8c9ab0fba1214995
23233 <1@qq.com>
2020-08-06 22:29:37 +0800

    爬虫监听器标识设置为可选项目

--- handler.go
+++ handler.go
@@ -361,8 +361,7 @@ func SpiderVisitHistoryMiddleware(ctx iris.Context) {
 		go func() {
 			ua := ctx.GetHeader("User-Agent")
 			// 判断ua是否是爬虫
-			spiderPrefix := []string{"spider", "Spider", "bot", "Bot", "crawler", "trident", "Trident", "Slurp", "request", "python", "node", "craw"}
-			for _, prefix := range spiderPrefix {
+			for _, prefix := range NowSpAdmin.config.SpiderMatchList {
 				if strings.Contains(ua, prefix) {
 					ip := realip.Get(ctx.Request())
 					var d SpiderHistory
